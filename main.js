6338bcbb87a4a718fe30490d27b4a7b2ffa49dbb
exradr <liamsaliba@hotmail.com>
2019-10-24 18:28:11 +1100

    New version!
    TODO: info for brownless, ge, law

---
+++ main.js
@@ -0,0 +1,34 @@
+$('#86-widget').click(function (e) {
+    $('#all-box').fadeOut('fast', function () {
+        $('#86-box').fadeIn('fast');
+    });
+});
+
+$('#abp-widget').click(function (e) {
+    $('#all-box').fadeOut('fast', function () {
+        $('#abp-box').fadeIn('fast');
+    });
+});
+
+$('#brownless-widget').click(function (e) {
+    $('#all-box').fadeOut('fast', function () {
+        $('#brownless-box').fadeIn('fast');
+    });
+});
+$('#ge-widget').click(function (e) {
+    $('#all-box').fadeOut('fast', function () {
+        $('#ge-box').fadeIn('fast');
+    });
+});
+
+$('#law-widget').click(function (e) {
+    $('#all-box').fadeOut('fast', function () {
+        $('#law-box').fadeIn('fast');
+    });
+});
+
+$('.back-btn').click(function (e) {
+    $('.other-widget').fadeOut('fast', function () {
+        $('#all-box').fadeIn('fast');
+    });
+});
